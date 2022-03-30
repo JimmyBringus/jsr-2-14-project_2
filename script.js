337520b2d3c73d785b60da1d3fc8bf06afb0579f
JimmyBringus <jbenson2008@gmail.com>
2022-03-30 14:11:40 -0400

    project update 3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const button = document.querySelector('button')
+const randomize = document.querySelector('.randomizeButton')
+const favourite = document.querySelector('.favouriteButton')
+const viewFaves = document.querySelector('.viewFavesButton')
 const imageDiv = document.querySelector('div')
 const innerQuote = document.querySelector('#textPaste')
 const innerAuthor = document.querySelector('#quoteAuthor')
@@ -9,7 +11,7 @@ function createClass(name,rules){
 
 }
 
-button.addEventListener('click', async () => {
+randomize.addEventListener('click', async () => {
     let response = await axios.get(
         `https://animechan.vercel.app/api/random`
     )
@@ -26,11 +28,18 @@ button.addEventListener('click', async () => {
     let imageId = `https://picsum.photos/id/${picsumId}/info`
     console.log(imageId)
 
-    document.querySelector('.imagePaste').setAttribute("style",`background-image: url('https://picsum.photos/id/${picsumId}/1920/1080.webp');`)
+    let quoteCreation = () => {
+        document.querySelector('.imagePaste').setAttribute("style",`background-image: url('https://picsum.photos/id/${picsumId}/1920/1080.webp');`)
 
     // imageDiv.innerHTML = `<style = "background-image: url('https://picsum.photos/id/${picsumId}}/1280/720?blur=1.webp');">`
     // document.querySelector("#randomImg").setAttribute("src",`https://picsum.photos/id/${picsumId}/1280/720.webp`)
-    innerQuote.innerText = response.data.quote
-    innerAuthor.innerHTML = `&mdash; ${response.data.character}, <i>${response.data.anime}</i>`
+        innerQuote.innerText = response.data.quote
+        innerAuthor.innerHTML = `&mdash; ${response.data.character}, <i>${response.data.anime}</i>`
+    }
+
+    quoteCreation()
 })
 
+favourite.addEventListener('click', () => {
+    
+})
